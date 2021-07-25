586cbb07c24cb0078e00e07fe9e4d3e4a3ed8dca
Danny Edel <mail@danny-edel.de>
2021-07-25 11:37:53 +0200

    support single-file installation

--- ansible/deploy-localhost.sh
+++ ansible/deploy-localhost.sh
@@ -1,6 +1,11 @@
 sudo true
+if [[ -z "$*" ]] ; then
+	ARGS=playbooks/*.yml
+else
+	ARGS="$@"
+fi
 ansible-playbook \
 	-c local --inventory ./inventory.yml \
 	--limit "$(hostname -f)" \
 	-vv \
-	playbooks/*.yml
+	$ARGS
